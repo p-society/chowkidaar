25c1067b1f03df0f1360280840d81ac90bb8751f
majorbruteforce <b122103@gmail.com>
2024-05-31 22:30:59 +0530

    validity checker

--- bot/db.py
+++ bot/db.py
@@ -1,5 +1,6 @@
 import psycopg2
 from config import DATABASE_NAME, DATABASE_USER, DATABASE_PASSWORD, DATABASE_HOST
+from parse_message import extract_user_info
 
 def connect_to_database():
     try:
@@ -28,16 +29,29 @@ def save_log(conn, message, discord_user_id, discord_message_id, sent_at ,in_tex
     conn.commit()
     cur.close()
 
+def check_intext_validity(conn, message):
+    try: 
+        college_id = extract_user_info(message)
+        if college_id:
+            cur= conn.cursor()
+            cur.execute("SELECT name FROM student_list_2023 WHERE student_id = %s", (college_id.upper(),))
+            full_name = cur.fetchone()
+            if full_name:
+                first_name = full_name[0].split()[0]  # Assuming name is the first element of the tuple
+                if first_name.lower() in message.lower():
+                    return 1
+                elif full_name[0].lower() in message.lower():
+                    return 1
+            else:
+                print(f"No name found for student_id: {college_id}")
+        return 0   
 
+    except Exception as e:
+        print(f"Error while checking intext validity: {e}")
+        return -1
 
 
 if __name__ == "__main__":
-    connection = connect_to_database()
-    if connection:
-        cursor = connection.cursor()
-        cursor.execute("SELECT version();")
-        db_version = cursor.fetchone()
-        print(f"Database version: {db_version}")
-        
-        cursor.close()
-        connection.close()
+    conn= connect_to_database()
+    print(check_intext_validity(conn, "Abhigyan Dutta B123003 aetwgwrgwsgrgwsrgswgs"))
+    conn.close()
--- bot/main.py
+++ bot/main.py
@@ -4,59 +4,60 @@ import signal
 import sys
 from config import DISCORD_TOKEN, WATCHED_CHANNEL_ID
 from discord.ext import commands
-from db import connect_to_database, save_log
-from parse_message import extract_user_info
+from db import connect_to_database, save_log, check_intext_validity
 import os
 
 conn = connect_to_database()
-cur= conn.cursor()
+cur = conn.cursor()
 
 intents = discord.Intents.default()
 intents.messages = True  # Ensure the bot can read messages
 intents.message_content = True  # Add this line if you need access to message content
 
-bot = commands.Bot(command_prefix='!', intents=intents)
+bot = commands.Bot(command_prefix="!", intents=intents)
+
 
 @bot.event
 async def on_ready():
-    print(f'Bot is ready. Logged in as {bot.user}')
+    print(f"Bot is ready. Logged in as {bot.user}")
+
 
 @bot.event
 async def on_message(message):
-    in_text_valid = -1
     if message.author == bot.user:
-        return  # Ignore messages from the bot itself
+        return
 
     if message.channel.id != WATCHED_CHANNEL_ID:
-        return  # Ignore messages not from the watched channel
-    
-    
-    college_id = extract_user_info(message.content)
-    print(college_id)
+        return
 
     discord_user_id = message.author.id
-    discord_message_id= message.id
+    discord_message_id = message.id
     content = str(message.content)
     timestamp = message.created_at
+    in_text_valid = check_intext_validity(conn, content)
 
-    try: 
+    try:
         if in_text_valid == 1:
-            save_log(conn, content, discord_user_id, discord_message_id, timestamp ,in_text_valid=-1)
+            save_log(
+                conn,
+                content,
+                discord_user_id,
+                discord_message_id,
+                timestamp,
+                in_text_valid,
+            )
             print(f"Message from {message.author.name} saved to the database.")
-            await message.add_reaction('🎊')
+            await message.add_reaction("🎊")
         else:
             print(f"Message from {message.author.name} is invalid")
-            await message.add_reaction('❌')
+            await message.add_reaction("❌")
 
     except Exception as e:
         print(f"Error saving message to database: {e}")
         conn.rollback()
- 
-
-
-
 
     await bot.process_commands(message)
+
 
 # async def shutdown(signal, frame):
 #     print("Shutting down bot and closing database connection...")
