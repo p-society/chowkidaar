bde5925907f66067dbd0e261c5757b873ec8e108
iamanishx <manishbiswal754@gmail.com>
2025-05-29 00:48:47 +0530

    smol-fix
    Signed-off-by: iamanishx <manishbiswal754@gmail.com>

--- daily_questions.py
+++ daily_questions.py
@@ -50,13 +50,15 @@ def create_daily_question_message(day_number, questions_list):
 
 # Calculate days since start date
 def get_current_day(start_date):
-    today = datetime.datetime.now(pytz.timezone.utc).date()
+    today = datetime.datetime.now(datetime.timezone.utc).date()
     delta = (today - start_date).days + 1  # +1 because we want day 1, 2, 3, etc.
     return delta if delta > 0 else 0
 
 # Get the first day of next month
 def get_start_date():
-    return datetime.datetime(2025, 5, 29, tzinfo=datetime.timezone.utc).date()
+    now = datetime.datetime.now()
+    print(f"Current date: {now}")
+    return datetime.datetime(2025, 5, 22, tzinfo=datetime.timezone.utc).date()
 
 
 class DailyQuestionScheduler:
