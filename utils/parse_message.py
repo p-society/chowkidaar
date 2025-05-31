5be0188dd849543e97c9a423cbe7a60a9d4b057e
iamanishx <manishbiswal754@gmail.com>
2025-05-31 22:42:20 +0530

    bug fixes
    Signed-off-by: iamanishx <manishbiswal754@gmail.com>

--- utils/parse_message.py
+++ utils/parse_message.py
@@ -1,4 +1,3 @@
-from utils.dfa import IdentityAutomata
 import re
 
 def extract_user_info(msg: str):
@@ -11,7 +10,6 @@ def extract_user_info(msg: str):
     Returns:
         tuple: (user_id, name) if found, (None, None) otherwise
     """
-    machine = IdentityAutomata()
     name = msg.split('\n')[0].strip()[5:]
     
     # Extract name (first line)
@@ -30,8 +28,7 @@ def extract_user_info(msg: str):
         match = re.search(pattern, msg)
         if match:
             college_id = match.group(1)
-            if machine.is_accepted(college_id[1:]):
-                return college_id, name
+            return college_id, name
                 
     return None, None
 
